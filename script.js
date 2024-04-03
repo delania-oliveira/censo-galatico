2cfb5ebe3185ddd87a087200c50b4a86bccfbff5
Delania Oliveira <delania.oliveiraa@gmail.com>
2024-04-03 10:54:38 -0300

    Obtem os dados de um planeta
    Adicionada a funcionalidade de exibir os dados do planeta.
    Agora é possível clicar no botão e ver as informações sobre o planeta específico.

--- script.js
+++ script.js
@@ -8,12 +8,31 @@ function getPlanets() {
 
       planets.forEach(planet => {
         const btn = document.createElement('button')
+        const planetUrl = planet.url
 
-        btn.innerHTML= `${planet.name}`
+        btn.innerHTML= `${planet.name}`        
+        btn.addEventListener('click', () => {handleDataPlanet(planetUrl)})
 
         navBar.appendChild(btn)
       })
-
     })
 }
 document.addEventListener('DOMContentLoaded', getPlanets)
+
+function handleDataPlanet(url){
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+            
+      const planetData = document.getElementById('planet-content')
+
+      planetData.innerHTML = `
+        <div class="planet-info">
+          <h2>${data.name}</h2>
+          <span><strong>População: </strong>${data.population}</span>
+          <span><strong>Clima: </strong>${data.climate}</span>
+          <span><strong>Terreno: </strong>${data.terrain}</span>
+        </div>
+      `
+    })
+}
